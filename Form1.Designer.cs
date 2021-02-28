c071ae0b9b4938a6f20960e0f39f63777871d018
Valdemar Subotkovski <s037070@ad.viko.lt>
2021-02-28 17:50:39 +0200

    Added cbc mode, changed GUI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,6 +44,13 @@ namespace AES.Info.Saugumas
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.ECBButton = new System.Windows.Forms.RadioButton();
+            this.CBCButton = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.encryptionIVtextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.decryptionIVtextBox = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -52,22 +59,24 @@ namespace AES.Info.Saugumas
             this.label1.Font = new System.Drawing.Font("Geometr415 Blk BT", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(249, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(302, 41);
+            this.label1.Size = new System.Drawing.Size(292, 41);
             this.label1.TabIndex = 0;
-            this.label1.Text = "AES ALGORYTHM";
+            this.label1.Text = "AES ALGORITHM";
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(22, 179);
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Location = new System.Drawing.Point(22, 216);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(335, 136);
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(377, 222);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 163);
+            this.label2.Location = new System.Drawing.Point(19, 200);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 2;
@@ -77,7 +86,7 @@ namespace AES.Info.Saugumas
             // 
             this.textBox1.Location = new System.Drawing.Point(22, 129);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(335, 20);
+            this.textBox1.Size = new System.Drawing.Size(377, 20);
             this.textBox1.TabIndex = 3;
             // 
             // label3
@@ -91,12 +100,13 @@ namespace AES.Info.Saugumas
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(282, 321);
+            this.button1.Location = new System.Drawing.Point(324, 444);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
             this.button1.Text = "Encrypt";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label4
             // 
@@ -111,7 +121,7 @@ namespace AES.Info.Saugumas
             // 
             this.textBox2.Location = new System.Drawing.Point(453, 129);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(335, 20);
+            this.textBox2.Size = new System.Drawing.Size(377, 20);
             this.textBox2.TabIndex = 6;
             // 
             // label5
@@ -128,7 +138,7 @@ namespace AES.Info.Saugumas
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Geometr415 Blk BT", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(609, 55);
+            this.label6.Location = new System.Drawing.Point(646, 55);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(184, 41);
             this.label6.TabIndex = 9;
@@ -136,33 +146,35 @@ namespace AES.Info.Saugumas
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(713, 321);
+            this.button2.Location = new System.Drawing.Point(755, 444);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 12;
             this.button2.Text = "Decrypt";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(450, 163);
+            this.label7.Location = new System.Drawing.Point(450, 200);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(75, 13);
+            this.label7.Size = new System.Drawing.Size(81, 13);
             this.label7.TabIndex = 11;
-            this.label7.Text = "Text to enrypt:";
+            this.label7.Text = "Text to decrypt:";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(453, 179);
+            this.richTextBox2.DetectUrls = false;
+            this.richTextBox2.Location = new System.Drawing.Point(453, 216);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(335, 136);
+            this.richTextBox2.Size = new System.Drawing.Size(377, 43);
             this.richTextBox2.TabIndex = 10;
             this.richTextBox2.Text = "";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(632, 321);
+            this.button3.Location = new System.Drawing.Point(674, 444);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 13;
@@ -171,18 +183,88 @@ namespace AES.Info.Saugumas
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(193, 321);
+            this.button4.Location = new System.Drawing.Point(235, 444);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(83, 23);
             this.button4.TabIndex = 14;
             this.button4.Text = "Copy from file";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ECBButton
+            // 
+            this.ECBButton.AutoSize = true;
+            this.ECBButton.Location = new System.Drawing.Point(64, 12);
+            this.ECBButton.Name = "ECBButton";
+            this.ECBButton.Size = new System.Drawing.Size(46, 17);
+            this.ECBButton.TabIndex = 15;
+            this.ECBButton.Text = "ECB";
+            this.ECBButton.UseVisualStyleBackColor = true;
+            // 
+            // CBCButton
+            // 
+            this.CBCButton.AutoSize = true;
+            this.CBCButton.Checked = true;
+            this.CBCButton.Location = new System.Drawing.Point(12, 12);
+            this.CBCButton.Name = "CBCButton";
+            this.CBCButton.Size = new System.Drawing.Size(46, 17);
+            this.CBCButton.TabIndex = 16;
+            this.CBCButton.TabStop = true;
+            this.CBCButton.Text = "CBC";
+            this.CBCButton.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(19, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "IV:";
+            // 
+            // encryptionIVtextBox
+            // 
+            this.encryptionIVtextBox.Location = new System.Drawing.Point(22, 168);
+            this.encryptionIVtextBox.Name = "encryptionIVtextBox";
+            this.encryptionIVtextBox.Size = new System.Drawing.Size(377, 20);
+            this.encryptionIVtextBox.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(450, 152);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "IV:";
+            // 
+            // decryptionIVtextBox
+            // 
+            this.decryptionIVtextBox.Location = new System.Drawing.Point(453, 168);
+            this.decryptionIVtextBox.Name = "decryptionIVtextBox";
+            this.decryptionIVtextBox.Size = new System.Drawing.Size(377, 20);
+            this.decryptionIVtextBox.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(453, 299);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox3.Size = new System.Drawing.Size(377, 20);
+            this.textBox3.TabIndex = 21;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 365);
+            this.ClientSize = new System.Drawing.Size(856, 484);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.decryptionIVtextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.encryptionIVtextBox);
+            this.Controls.Add(this.CBCButton);
+            this.Controls.Add(this.ECBButton);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -223,6 +305,13 @@ namespace AES.Info.Saugumas
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton ECBButton;
+        private System.Windows.Forms.RadioButton CBCButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox encryptionIVtextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox decryptionIVtextBox;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,19 +8,149 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
+using System.IO;
 
 namespace AES.Info.Saugumas
 {
     public partial class Form1 : Form
     {
+        private AesCryptoServiceProvider provider;
+        private Encoding encoding = Encoding.GetEncoding("437");
+        //private Encoding encoding = Encoding.Unicode;
+
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = "adsfhfgdsfds3254";
+            encryptionIVtextBox.Text = "jdue758j";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (fileDialog.ShowDialog(this) == DialogResult.OK)
+                richTextBox1.Text = System.IO.File.ReadAllText(fileDialog.FileName.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                provider = new AesCryptoServiceProvider();
+                //provider.Padding = PaddingMode.PKCS7;
+                //provider.BlockSize = 128; //128 bits
+                // provider.KeySize = 256; //128, 192 or 256 bits
+                validateInputDataForDecryption();
+                decryption(textBox2.Text, richTextBox2.Text, decryptionIVtextBox.Text);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                provider = new AesCryptoServiceProvider();
+                //provider.Padding = PaddingMode.PKCS7;
+                //provider.BlockSize = 128; //128 bits
+                //provider.KeySize = 256; //128, 192 or 256 bits
+                validateInputDataForEncryption();
+                encryption(textBox1.Text, richTextBox1.Text, encryptionIVtextBox.Text);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void encryption(string encryptionKey, string encryptionText, string IValue)
+        {
+            byte[] encryptionTextBytes = encoding.GetBytes(encryptionText);
+            provider.Key = encoding.GetBytes(encryptionKey);
+            provider.IV = encoding.GetBytes(IValue);
+
+            if (CBCButton.Checked)
+                provider.Mode = CipherMode.CBC;
+            if (ECBButton.Checked)
+                provider.Mode = CipherMode.ECB;
+            /*
+            using (provider)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, provider.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(encryptionTextBytes, 0, encryptionTextBytes.Length);
+                    }
+                    richTextBox2.Text = encoding.GetString(ms.ToArray());
+                    textBox3.Text = encoding.GetString(ms.ToArray());
+                }
+            }
+            */
+            ICryptoTransform icrypt = provider.CreateEncryptor(provider.Key, provider.IV);
+            byte[] encryptedTextBytes = icrypt.TransformFinalBlock(encryptionTextBytes, 0, encryptionTextBytes.Length);
+            icrypt.Dispose();
+            richTextBox2.Text = encoding.GetString(encryptedTextBytes);
+            textBox3.Text = encoding.GetString(encryptedTextBytes);
+        }
+
+        private void decryption(string decryptionKey, string decryptionText, string IValue)
+        {
+            byte[] decryptionTextBytes = encoding.GetBytes(decryptionText);
+            provider.Key = encoding.GetBytes(decryptionKey);
+            provider.IV = encoding.GetBytes(IValue);
+
+            if (CBCButton.Checked)
+                provider.Mode = CipherMode.CBC;
+            if (ECBButton.Checked)
+                provider.Mode = CipherMode.ECB;
+            /*
+            using (provider)
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, provider.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(decryptionTextBytes, 0, decryptionTextBytes.Length);
+                    }
+                    richTextBox1.Text = encoding.GetString(ms.ToArray());
+                }
+            }*/
+            
+            ICryptoTransform icrypt = provider.CreateDecryptor(provider.Key, provider.IV);
+            byte[] decryptedTextBytes = icrypt.TransformFinalBlock(decryptionTextBytes, 0, decryptionTextBytes.Length);
+            icrypt.Dispose();
+            richTextBox1.Text = encoding.GetString(decryptedTextBytes);
+            
+        }
+         
+        private void validateInputDataForEncryption()
+        {
+            if (String.IsNullOrWhiteSpace(textBox1.Text))
+                throw new Exception("Plese enter the key value!");
+            if (String.IsNullOrWhiteSpace(encryptionIVtextBox.Text))
+                throw new Exception("Plese enter the IV value!");
+            if (String.IsNullOrWhiteSpace(richTextBox1.Text))
+                throw new Exception("Plese fill in text field!");
+        }
+
+        private void validateInputDataForDecryption()
+        {
+            if (String.IsNullOrWhiteSpace(textBox2.Text))
+                throw new Exception("Plese enter the key value!");
+            if (String.IsNullOrWhiteSpace(decryptionIVtextBox.Text))
+                throw new Exception("Plese enter the IV value!");
+            if (String.IsNullOrWhiteSpace(richTextBox2.Text))
+                throw new Exception("Plese fill in text field!");
         }
     }
 }
